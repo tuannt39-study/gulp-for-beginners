ba5578cbedb467fd86867b185749d2d52208f2ae
tuannt39 <tuannt39@gmail.com>
2018-02-24 16:45:51 +0700

    all sass to css

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,9 @@ gulp.task('sass', function(){
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('app/css'))
 });
+
+gulp.task('sassall', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+});
