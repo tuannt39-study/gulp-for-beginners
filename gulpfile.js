e2302838a45098a8e10d1a050f0099a92ed75ff4
tuannt39 <tuannt39@gmail.com>
2018-02-24 15:30:16 +0700

    sass to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
 gulp.task('hello', function() {
   console.log('Xin chào Tuân');
+});
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
 });
