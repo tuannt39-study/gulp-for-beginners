100c2c27cb957161e8c35723b491cff5384900c3
tuannt39 <tuannt39@gmail.com>
2018-02-24 17:11:03 +0700

    browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('hello', function() {
   console.log('Xin chào Tuân');
+});
+
+gulp.task('watch', ['browserSync', 'sassall'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sassall']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 });
 
 gulp.task('sass', function(){
@@ -17,9 +25,20 @@ gulp.task('sassall', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 gulp.task('watchsass', function(){
   gulp.watch('app/scss/**/*.scss', ['sassall']);
   // Other watchers
 });
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
